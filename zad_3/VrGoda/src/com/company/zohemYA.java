8be928ecac76445f67c26ce9c2b21195f05afa34
Neon Rice <112954242+NeonRiceNon@users.noreply.github.com>
2022-09-30 20:30:06 +0300

    Add files via upload

---
+++ zad_3/VrGoda/src/com/company/zohemYA.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class zohemYA extends VrGodaName {
+    // пункт 6.2
+@Override
+    public  void getDescription (String a){
+        a = "Холодное время года";
+        return;
+    }
+}
